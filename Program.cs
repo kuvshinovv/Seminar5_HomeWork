b6c9665514296f7aa2e183eae0133c510e179954
Kuvshinov Vlad <kuvshinov_v@mail.ru>
2022-09-02 21:01:10 +0300

    Добавил решение второй задачи

--- Program.cs
+++ Program.cs
@@ -2,13 +2,12 @@
 //которая покажет количество чётных чисел в массиве.
 //[345, 897, 568, 234] -> 2
 
-int min = 0;  // определяем переменные
+/* int min = 0;  // определяем переменные
 int max = 0;
 int size = 0;
 
 int[] CreateRandomArray(int size, int min, int max){   // Создаем рандомный массив
     int[] array = new int[size];
-    
         for (int i = 0; i < size; i++)
         {
             array[i] = new Random().Next(min, max+1);
@@ -49,7 +48,7 @@ int InputMIN(){
             Console.WriteLine("Введите МИНИМАЛЬНЫЙ трёхзначный элемент массива: ");
             min = Convert.ToInt32(Console.ReadLine());
         }
-        return min;
+    return min;
 }
 int InputMAX(){
     Console.WriteLine("Введите МАКСИМАЛЬНЫЙ трёхзначный элемент массива: ");
@@ -72,12 +71,65 @@ InputMAX();
 int[] array = CreateRandomArray(size, min, max);
 ShowArray(array);
 Console.WriteLine();
-CountChet(array);
+CountChet(array); */
 
 
 //Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 //[3, 7, 23, 12] -> 19
 //[-4, -6, 89, 6] -> 0
+
+
+int min = 0;  // определяем переменные
+int max = 0;
+int size = 0;
+int sum = 0;
+
+int[] CreateRandomArray(int size, int min, int max){   // Создаем рандомный массив
+    int[] array = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = new Random().Next(min, max+1);
+        }
+    return array;
+}
+
+void ShowArray(int[] array){        // метод выведения на экран созданного массива
+    Console.Write($"[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+" ");
+    }
+    Console.Write($"]");
+}
+
+void SummNechetPos(int[] array){
+    
+    for (int i = 0; i < array.Length; i=i+2) {
+        sum = sum + array[i];
+    }
+    Console.Write($" Сумма элементов, стоящих на нечётных позициях = {sum} ");
+}
+
+int InputMAX(){
+    Console.WriteLine("Введите МАКСИМАЛЬНЫЙ элемент массива: ");
+    max = Convert.ToInt32(Console.ReadLine());
+        if (max < min){
+            Console.WriteLine("Вы ввели максимум, который меньше минимума, попробуйте снова...");
+            InputMAX();
+        }
+    return max;
+}
+
+Console.WriteLine("Введите значение размера массива: ");
+size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите МИНИМАЛЬНОЕ значение массива: ");
+min = Convert.ToInt32(Console.ReadLine());
+InputMAX();
+
+int[] array = CreateRandomArray(size, min, max);
+
+ShowArray(array);
+SummNechetPos(array);
 
 
 
