9ded062f3d75217b6f03ea5a1352a264d5d71339
Kuvshinov Vlad <kuvshinov_v@mail.ru>
2022-09-02 22:35:59 +0300

    Добавил решение третьей задачи

--- Program.cs
+++ Program.cs
@@ -79,7 +79,7 @@ CountChet(array); */
 //[-4, -6, 89, 6] -> 0
 
 
-int min = 0;  // определяем переменные
+/* int min = 0;  // определяем переменные
 int max = 0;
 int size = 0;
 int sum = 0;
@@ -130,9 +130,100 @@ int[] array = CreateRandomArray(size, min, max);
 
 ShowArray(array);
 SummNechetPos(array);
-
+ */
 
 
 
 //Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 //[3 7 22 2 78] -> 76
+
+int min = 0;  // определяем переменные
+int max = 0;
+int size = 0;
+int maxElement = 0;
+int minElement = 0;
+bool pass = false;
+
+int[] CreateRandomArray(int size, int min, int max){   // Создаем рандомный массив
+    int[] array = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = new Random().Next(min, max+1);
+        }
+    return array;
+}
+
+void ShowArray(int[] array){        // метод выведения на экран созданного массива
+    Console.Write($"[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+" ");
+    }
+    Console.Write($"]");
+}
+
+int MaxEL(int[] array){
+    maxElement = array[0];
+        for (int i = 0; i < array.Length; i++) {
+            if (array[i] > maxElement){
+                maxElement = array[i];
+            }
+        } 
+    return  maxElement;
+}
+
+int MinEL(int[] array){
+    minElement = array[0];
+        for (int i = 0; i < array.Length; i++) {
+            if (array[i] < minElement){
+                minElement = array[i];
+            }
+        } 
+    return  minElement;  
+}
+
+int InputSIZE(){
+    Console.WriteLine("Введите значение размера массива: ");
+    size = Convert.ToInt32(Console.ReadLine());
+        if(size>0){
+           pass = true;  // т.к. размер массива больше 0 то ставим в pass значение true и в дальнейшем продолжем ввдить данные.
+        }
+        else if(size<0){
+            Console.WriteLine("Размер массива не может быть отрицательным! ");
+            InputSIZE();  // использовал рекурсию (вызов функции самой себя)
+        }
+        else if(size==0){
+            Console.WriteLine("Размер массива = 0! Минимальных и Максимальных значений нет!!! ");
+            pass = false;
+        }
+    return size;
+}
+
+int InputMAX(){
+        Console.WriteLine("Введите МАКСИМАЛЬНЫЙ элемент массива: ");
+        max = Convert.ToInt32(Console.ReadLine());
+            if (max < min){
+                Console.WriteLine("Вы ввели максимум, который меньше минимума, попробуйте снова...");
+                InputMAX();  // использовал рекурсию (вызов функции самой себя)
+            }
+        return max;
+    }
+
+
+InputSIZE();
+if (pass == true){   // pass работает как флаг..  если он true (т.е введенный размер массива > 0, то тогда продолжаем вводить мин и макс...  
+                    //если он false то останавливаем программу и говорим что размер массива = 0 )
+
+    Console.WriteLine("Введите МИНИМАЛЬНОЕ значение массива: ");   // этот ввод не стал делать методом, т.к. его значение может быть любым
+    min = Convert.ToInt32(Console.ReadLine());
+    
+    InputMAX();   // вызываем функци. ввода максимального числа
+
+    int[] array = CreateRandomArray(size, min, max);  // создаем массив и кладем в переменную 
+
+    ShowArray(array); // выводим созданный массив
+
+    int maxel = MaxEL(array);  // вызываем функцию которая ищет максимальное число
+    int minel = MinEL(array);   // вызываем функцию которая ищет минимальное число
+    Console.WriteLine($" Разница между максимальным ({maxel}) и минимальным ({minel}) элементом = {maxel-minel} ");
+}
